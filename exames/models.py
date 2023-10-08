a30cea03ce26511a8369d68ebc627e59dedbb2e6
Gropp <fgropp@gmail.com>
2023-10-08 19:40:16 -0300

    Gerar acesso medico

--- exames/models.py
+++ exames/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+from datetime import timedelta
 from django.utils.safestring import mark_safe
+from secrets import token_urlsafe
 
 # Create your models here.
 # Herdam a classe do models.model - geram os sqls
@@ -59,3 +62,48 @@ class PedidosExames(models.Model):
 
     def __str__(self):
       return f'{self.usuario} | {self.data}'
+
+class AcessoMedico(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    identificacao = models.CharField(max_length=50)
+    tempo_de_acesso = models.IntegerField() # Em horas
+    criado_em = models.DateTimeField()
+    data_exames_iniciais = models.DateField()
+    data_exames_finais = models.DateField()
+    token = models.CharField(max_length=20, null=True, blank=True)
+
+    def __str__(self):
+        return self.token
+    
+    # sobreescreve o metodo SAVE da MODELS para esse modelo AcessoMedico
+    # acrescenta ao metodo salvar a geracao de um token ANTES do salvamento
+    def save(self, *args, **kwargs):
+        if not self.token:
+            # esse token serve para garantir a origem da requesicao
+            self.token = token_urlsafe(6)
+        # no final ele executa o SAVE da bibliote PAI (original)
+        # nao contem caracteres inseguros para usar na URL
+        super(AcessoMedico, self).save(*args, **kwargs)
+
+    # como esse metodo tem apenas um retorno, para nao precisarmos chama-lo acesso_medico.status(), com parenteses, entao com um decorador podemos transforma-lo em uma propriedade do acesso_medico e chama-lo direto acesso_medico.status
+    @property
+    # até a cor altera no vscode
+    def status(self):
+        # verificar se o timezone > que a data de criacao
+        # timedelta permite colocar um valor em horas que pode ser somado a data
+        # neste caso estamos passando o tempo de acesso liberado para o medico
+        # if timezone.now() > (self.criado_em + timedelta(hours=self.tempo_de_acesso)):
+        #     return 'Expirado'
+        # else:
+        #     return 'Ativo'
+
+        #  PODEMOS FAZER O IF ACIMA DE FORMA DIRETA:
+        return 'Expirado' if timezone.now() > (self.criado_em + timedelta(hours=self.tempo_de_acesso)) else 'Ativo'
+
+    @property
+
+    def url(self):
+        return f'http://127.0.0.1:8000/exames/acesso_medico/{self.token}'
+    # para aplicar essa classe rodar no terminal:
+    # python3 manage.py makemigrations
+    # python3 manage.py migrate
--- exames/views.py
+++ exames/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 # IMPORTANDO UM DECORET DO PYTHON, PODEMOS FAZE QUE DE FORMA AUTOMATICA SOMENTE USUARIOS LOGADOS ACESSEM ESSA VIEW, SEM A NECESSIDADE DE CODIGOS DE TESTE
 from django.contrib.auth.decorators import login_required
 # importamos o model do banco de dados TiposExames, PedidosExames e SolicitacaoExame
-from .models import TiposExames, PedidosExames, SolicitacaoExame
+from .models import TiposExames, PedidosExames, SolicitacaoExame, AcessoMedico
 from datetime import datetime
 from django.contrib import messages
 from django.contrib.messages import constants
@@ -165,3 +165,48 @@ def solicitar_senha_exame(request, exame_id):
             messages.add_message(request, constants.ERROR, 'Senha Invalida.')
             # sempre ao redirecionar para um html com argumento, utilize o f antes do path
             return redirect(f'/exames/solicitar_senha_exame/{exame.id}')
+
+@login_required
+
+def gerar_acesso_medico(request):
+    if request.method == "GET":
+        # O HTMLprecisa de dados do acesso medico existentes no banco para preencher uma tabela ao carregar a pagina
+        # entao criamos um objeto com todos os dados do banco AcessoMedico com o usuario LOGADO como filtro
+        acessos_medico = AcessoMedico.objects.filter(usuario=request.user)
+        # passa a instancia do SolicitarExame - exame como context da URL
+        # assim é possivel referenciar a tabela e tabelas relacionadas no HTML
+        # enviamos esse objeto para o HTML durante a operacao de RENDER com o alias acessos_medico
+        return render(request, 'gerar_acesso_medico.html', {'acessos_medico':acessos_medico})
+    elif request.method == "POST":
+        # recebe do HTML TODOS os dados do formulario gerar acesso medico
+        identificacao = request.POST.get('identificacao')
+        tempo_de_acesso = request.POST.get('tempo_de_acesso')
+        data_exame_inicial = request.POST.get('data_exame_inicial')
+        data_exame_final = request.POST.get('data_exame_final')
+        
+        # instancia o modelo
+        acesso_medico = AcessoMedico(
+            usuario = request.user,
+            identificacao = identificacao,
+            tempo_de_acesso = tempo_de_acesso,
+            data_exames_iniciais = data_exame_inicial,
+            data_exames_finais = data_exame_final,
+            criado_em = datetime.now()
+        )
+
+        # apos instanciar e carregar as variaveis, salva no banco de dados:
+        acesso_medico.save()
+
+        messages.add_message(request, constants.SUCCESS, 'Acesso cadastrado com sucesso.')
+        return redirect('/exames/gerar_acesso_medico/')
+
+# o medico só quer acessar o exame através do TOKEN, sem fazer login
+def acesso_medico(request, token):
+    acesso_medico = AcessoMedico.objects.get(token=token)
+    if acesso_medico.status == "Expirado":
+        messages.add_message(request, constants.ERROR, 'Esse Token já expirou, solicite outro.')
+        return redirect('/usuarios/login')
+    # colocando dois filtros seria como um e.
+    pedidos = PedidosExames.objects.filter(data__gte = acesso_medico.data_exames_iniciais).filter(data__lte=acesso_medico.data_exames_finais).filter(usuario=acesso_medico.usuario)
+    
+    return render(request, 'acesso_medico.html', {'pedidos': pedidos})
